41986e0b3c55fbd132407d8ce7ba47fe4fbbe217
rustling_leaves <rustling.tealeaves@gmail.com>
2026-01-15 12:16:33 +0200

    Opening a window and drawing
    It opens a window and draws an expanding ball on it. The window can be closed without crashing! :D

---
+++ RaylibTesti/RaylibTesti/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+
+namespace RaylibTesti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "Raylib");
+            while(Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.SkyBlue);
+                Raylib.DrawCircle(
+                    Raylib.GetScreenWidth() / 2,
+                    Raylib.GetScreenHeight() / 2,
+                    1 + (float)Raylib.GetTime() * 4, 
+                    Color.Violet);
+
+                Raylib.DrawCircleV(
+                    Raylib.GetScreenCenter(), 10,
+                    Color.Pink);
+                // Kaiken piirtämisen jälkeen
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
