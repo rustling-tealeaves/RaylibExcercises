b3850ff6a811a2068b57476485224334c3234b0f
rustling_leaves <rustling.tealeaves@gmail.com>
2026-01-28 09:54:05 +0200

    Add Raylib-cs NuGet and base code
    Added the nugget. Wrote what's basically the base starting point for all of these projects.

--- src/Pong/Program.cs
+++ src/Pong/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Pong
+﻿using Raylib_cs;
+
+namespace Pong
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(800, 600, "PONK!");
+            Raylib.SetTargetFPS(60);
+            while(Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
