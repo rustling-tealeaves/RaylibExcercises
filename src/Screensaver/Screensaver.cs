a04c6f6d13af7ed07964ca186c100932aa2cec6d
rustling_leaves <rustling.tealeaves@gmail.com>
2026-03-03 14:06:27 +0200

    Screensaver, define corners
    Instead of just declaring variables, also define a starting point for them.

--- src/Screensaver/Screensaver.cs
+++ src/Screensaver/Screensaver.cs
@@ -8,9 +8,10 @@ namespace Screensaver
         static void Main(string[] args)
         {
             // Declaring variables for the three different corners of the triangle
-            Vector2 cornerA;
-            Vector2 cornerB;
-            Vector2 cornerC;
+            // Also defining their locations
+            Vector2 cornerA = new Vector2 (Raylib.GetScreenWidth()/2, 40);
+            Vector2 cornerB = new Vector2 (40, Raylib.GetScreenHeight()/2);
+            Vector2 cornerC = new Vector2(Raylib.GetScreenWidth() - 40, (float)(Raylib.GetScreenHeight() * 0.75));
 
             Raylib.InitWindow(800, 800, "Thine Fancy Application for Sparing Monitors");
             Raylib.SetTargetFPS(30);
